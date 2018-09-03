3b29069e8ce2b9645044297df92788c5e17fa7ff
Philip Langer <planger@eclipsesource.com>
2018-09-03 12:26:31 +0200

    Nudge also first element on insertion of execution #343
    This change addresses comment #343#issuecomment-418037298 (third
    comment) of #343. When inserting an execution specification as first
    element (execParams.isInsertBefore) of a lifeline, the isInsertBefore
    flag has not been respected when invoking the nudge command.
    
    Signed-off-by: Philip Langer <planger@eclipsesource.com>

--- tests/org.eclipse.papyrus.uml.interaction.model.tests/src/org/eclipse/papyrus/uml/interaction/model/tests/SeqDCustomTests.java
+++ tests/org.eclipse.papyrus.uml.interaction.model.tests/src/org/eclipse/papyrus/uml/interaction/model/tests/SeqDCustomTests.java
@@ -14,6 +14,7 @@ package org.eclipse.papyrus.uml.interaction.model.tests;
 
 import org.eclipse.papyrus.uml.interaction.internal.model.spi.impl.tests.DefaultLayoutHelperTest;
 import org.eclipse.papyrus.uml.interaction.internal.model.spi.impl.tests.LogicalModelAdapterTest;
+import org.eclipse.papyrus.uml.interaction.model.tests.creation.CreateExecutionTest;
 import org.eclipse.papyrus.uml.interaction.model.tests.creation.CreateMessageTest;
 import org.eclipse.papyrus.uml.interaction.model.tests.creation.CreateMessageTestB;
 import org.eclipse.papyrus.uml.interaction.model.tests.deletion.BasicDeletionTest;
@@ -33,6 +34,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({ //
 		DefaultLayoutHelperTest.class, //
 		CreateMessageTest.class, CreateMessageTestB.class, //
+		CreateExecutionTest.class, //
 		CreationMessageDeletionTest.class, BasicDeletionTest.class, DeletionMessageDeletionTest.class, //
 		LogicalModelAdapterTest.class, //
 		LogicalModelPredicatesTest.class, //
